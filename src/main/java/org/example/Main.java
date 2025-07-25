21e45867c976b629ad4a403596f937809613ccaa
thaiTa27 <s4101740@student.rmit.edu.au>
2025-07-25 11:16:30 +1000

    lil bro

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,10 +9,10 @@ public class Main {
         bubbleSort(arr);
 
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+        for (int j : arr) {
+            System.out.print(j);
         }
-        System.out.println("end");
+        System.out.println("hahah");
     }
 
     public static void bubbleSort(int[] arr) {
